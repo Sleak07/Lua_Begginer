118bab1c23b1f351f3e3e6293acb0fee044c3509
Bimal Mallick <ghostridergothan8460@gmail.com>
2026-07-15 18:07:04 +0530

    Loops done

--- main.lua
+++ main.lua
@@ -1,1 +1,35 @@
 print("Hello World")
+
+--Functions in lua
+--Getting max of two numbers
+function Max(num1, num2)
+	if num1 > num2 then
+		Result = num1
+	else
+		Result = num2
+	end
+	return Result
+end
+
+print(Max(5, 2))
+
+--Loops in lua
+--while Loops
+local a = 10
+while a < 20 do
+	print("value of a:", a)
+	a = a + 1
+end
+
+--for Loops
+for i = 10, 19, 1 do
+	print(i)
+end
+
+--repeat Loops
+local i = 5
+repeat
+	i = i - 1
+
+until i == 1
+print(i)
